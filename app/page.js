e3c60d7179eebc9cdc8762909e06d4baf4523362
Arya Singh <arya30923@gmail.com>
2024-01-08 14:20:21 +0530

    .

--- app/page.js
+++ app/page.js
@@ -1,19 +1,62 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 
 const page = () => {
+  const [title, settitle] = useState("")
+  const [desc, setdesc] = useState("")
+  const [mainTask, setMainTask]= useState([])
+
+
+  const submitHandler = (e)=>{
+    e.preventDefault();
+    setMainTask([...mainTask, { title, desc }]);
+    console.log("title")
+    console.log("desc")
+    settitle("")
+    setdesc("")
+  }
+  let renderTask = <h2>No Task Available</h2>;
+if(mainTask.length>0){
+  renderTask = mainTask.map((t,i)=> {
+    return (
+      <li>
+    <div className="flex justify-between mb-5">
+      <h5 className="text-2xl font-semibold">{t.title}</h5>
+      <h6 className="text-xl font-semibold">{t.desc}</h6>
+    </div>
+    </li>
+    );
+  });
+}
   return (
     <>
     <h1 className='bg-black text-white p-5 text-5xl font-bold text-center'>Arya's Todo List</h1>
-    <form>
+    <form onSubmit={submitHandler}>
       <input type="text" className='text-2xl border-zinc-800 border-2 m-8 px-4 py-2'
-      placeholder='Enter task here'/>
+      placeholder='Enter title here' 
+      value={title}
+      onChange={(e)=>{
+        settitle(e.target.value)
+      }}
+      />
       <input type="text" className='text-2xl border-zinc-800 border-2 m-8 px-4 py-2'
-      placeholder='Enter description here'/>
-      <button className='bg-black text-white px-4 py-3 text-2xl font-bold rounded margin-4'>
-        Add Task 1</button>
+      placeholder='Enter description here'
+      value={desc}
+      onChange={(e)=>{
+        setdesc(e.target.value)
+      }}/>
+      <button className='bg-black text-white px-4 py-3 text-2xl font-bold rounded m-5'>
+        Add Task</button>
       </form>
-    </>
-  )
-}
+      <hr />
+      <div className="p-8 bg-slate-200">
+        <ul>
+          {renderTask}
+        </ul>
 
-export default page
+      </div>
+    </>
+  );
+}
+export default page;
+
